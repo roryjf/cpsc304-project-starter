a932bfcfcde51e6a9666d0325ef93ec0823e0067
roryjf <roryjf@outlook.com>
2018-03-26 23:49:12 -0700

    Fixed bugs in schema descriptions - set up dummy data (needs to be cleaned up, there are some very weird test entries)

--- db/SetupTables.sql
+++ db/SetupTables.sql
@@ -7,57 +7,20 @@ DROP TABLE IF EXISTS managers;
 DROP TABLE IF EXISTS coaches;
 DROP TABLE IF EXISTS staffInTeam;
 
-
-CREATE TABLE staffInTeam (
-    staffId char(5),
-    sName char(20),
-    teamId int NOT NULL,
-    PRIMARY KEY (staffId),
-    FOREIGN KEY (teamId)
-        REFERENCES Teams
-	        ON DELETE NO ACTION
-	        ON UPDATE CASCADE
-);
-
-CREATE TABLE coaches (
-    staffId int,
-    title char(20),
-    PRIMARY KEY (staffId),
-    FOREIGN KEY (staffId)
-        REFERENCES staffInTeam
-);
-
-CREATE TABLE managers (
-    staffId int,
-    teamId int,
-    PRIMARY KEY (staffId),
-    FOREIGN KEY (staffId)
-        REFERENCES staffInTeam
-);
-
-
-CREATE TABLE playersInTeam (
-    pname char(20),
-    jerseyNum int,
-    position char(3),
-    ratings double(1, 2),
-    fouls int,
-    onGoal% int,
-    goals int,
-    assists int,
-    teamId int NOT NULL,
-    PRIMARY KEY (pname, jerseyNum),
-    FOREIGN KEY (teamId)
-        REFERENCES teamsInLeague
+CREATE TABLE leagues (
+ 	leagueName char(20),
+ 	country char(20),
+    numTeams int,
+    PRIMARY KEY (leaguename, country)
 );
 
 CREATE TABLE teamsInLeague (
  	tName char(20),
-	teamId int,
+	teamId char(5),
 	wins int,
 	losses int,
 	draws int,
-    ratings double(1, 2),
+    ratings decimal(2),
     goals int,
     totalYellowCards int,
     totalRedCards int,
@@ -69,38 +32,71 @@ CREATE TABLE teamsInLeague (
         REFERENCES leagues
 );
 
+CREATE TABLE playersInTeam (
+    pname char(20),
+    jerseyNum int,
+    position char(3),
+    ratings decimal(2),
+    fouls int,
+    onGoalPercentage int,
+    goals int,
+    assists int,
+    teamId char(5) NOT NULL,
+    PRIMARY KEY (pname, jerseyNum),
+    FOREIGN KEY (teamId)
+        REFERENCES teamsInLeague
+);
 
-CREATE TABLE leagues (
- 	leaguename char(20),
- 	country char(20),
-    numTeams int,
-    PRIMARY KEY (leaguename, country)
-        ON DELETE CASCADE
+CREATE TABLE staffInTeam (
+    staffId char(5),
+    sName char(20),
+    teamId char(5) NOT NULL,
+    PRIMARY KEY (staffId),
+    FOREIGN KEY (teamId)
+        REFERENCES teamsInLeague
+	        ON DELETE NO ACTION
+	        ON UPDATE CASCADE
+);
+
+CREATE TABLE coaches (
+    staffId char(5),
+    title char(20),
+    PRIMARY KEY (staffId),
+    FOREIGN KEY (staffId)
+        REFERENCES staffInTeam
+);
+
+CREATE TABLE managers (
+    staffId char(5),
+    teamId char(5),
+    PRIMARY KEY (staffId),
+    FOREIGN KEY (staffId)
+        REFERENCES staffInTeam
 );
 
 CREATE TABLE matchesPlayedByTeams(
  	matchId int,
  	location char(20),
-	teamId int,
+	teamId char(5),
     result char(4) NOT NULL,
  	time int,
 	date date,
  	goals int,
 	numyellowCards int,
 	numredCards int,
-	PRIMARY KEY (teamId, matchId)
+	PRIMARY KEY (teamId, matchId),
     FOREIGN KEY (teamId)
         REFERENCES teamsInLeague
 	        ON DELETE CASCADE
 );
 
 CREATE TABLE updatedBy (
- 	staffId int,
+ 	staffId char(5),
  	uname char (20) NOT NULL,
  	jerseyNum int NOT NULL,
-    PRIMARY KEY (staffId, uname, jerseyNum)
+    PRIMARY KEY (staffId, uname, jerseyNum),
     FOREIGN KEY (staffId)
         REFERENCES managers,
     FOREIGN KEY (uname, jerseyNum)
-        REFERENCES PlayersInLeague
+        REFERENCES playersInTeam
 );
