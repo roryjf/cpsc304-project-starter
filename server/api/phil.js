38ffbab2900c8bc1f9f9b728b34fa9e1d9590894
Philippe Chapdelaine <37817723+philchapdelaine@users.noreply.github.com>
2018-03-29 17:10:00 -0700

    added a list

---
+++ server/api/phil.js
@@ -0,0 +1,12 @@
+team.js
+const team = Math.floor((Math.random() * 99999) + 11111);
+const name = req.body.
+const l_name =
+const l_country =
+const draws =
+const wins = req.body.params.wins
+const losses =
+const ratings =
+const total_yellow_cards =
+const total_red_cards =
+const query =
