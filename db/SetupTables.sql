b3d7318c5468d934a92dbc59c12be0f2c882fb10
Maskz1 <o4d0b@outlook.com>
2018-03-29 23:25:41 -0700

    Final Changes

--- db/SetupTables.sql
+++ db/SetupTables.sql
@@ -1,11 +1,11 @@
-DROP TABLE IF EXISTS updatedBy;
-DROP TABLE IF EXISTS matchesPlayedByTeams;
+DROP TABLE IF EXISTS updated_by;
+DROP TABLE IF EXISTS matches_played_by_teams;
 DROP TABLE IF EXISTS leagues;
-DROP TABLE IF EXISTS teamsInLeague;
-DROP TABLE IF EXISTS playersInTeam;
+DROP TABLE IF EXISTS teams_in_league;
+DROP TABLE IF EXISTS players_in_team;
 DROP TABLE IF EXISTS managers;
 DROP TABLE IF EXISTS coaches;
-DROP TABLE IF EXISTS staffInTeam;
+DROP TABLE IF EXISTS staff_in_team;
 
 CREATE TABLE leagues (
  	league_name char(20),
@@ -14,25 +14,25 @@ CREATE TABLE leagues (
     PRIMARY KEY (league_name, country)
 );
 
-CREATE TABLE teamsInLeague (
+CREATE TABLE teams_in_league (
     t_name char(20) NOT NULL,
- 	team_id char(5) NOT NULL,
-	wins int,
-	losses int,
-	draws int,
+    team_id char(5) NOT NULL,
+    wins int,
+    losses int,
+    draws int,
     ratings decimal(2),
     goals int,
     totalYellowCards int,
     totalRedCards int,
     standing int,
     league_name char(20) NOT NULL,
- 	country char(20) NOT NULL,
+    country char(20) NOT NULL,
     PRIMARY KEY (t_name),
     FOREIGN KEY (league_name, country)
         REFERENCES leagues
 );
 
-CREATE TABLE playersInTeam (
+CREATE TABLE players_in_team (
     pname char(20),
     jerseyNum int,
     position char(3),
@@ -44,7 +44,7 @@ CREATE TABLE playersInTeam (
     t_name char(20) NOT NULL,
     PRIMARY KEY (pname, jerseyNum),
     FOREIGN KEY (t_name)
-        REFERENCES teamsInLeague
+        REFERENCES teams_in_league
 );
 
 CREATE TABLE staffInTeam (
@@ -53,7 +53,7 @@ CREATE TABLE staffInTeam (
     t_name char(20) NOT NULL,
     PRIMARY KEY (staff_id),
     FOREIGN KEY (t_name)
-        REFERENCES teamsInLeague
+        REFERENCES teams_in_league
 	        ON DELETE NO ACTION
 	        ON UPDATE CASCADE
 );
@@ -63,7 +63,7 @@ CREATE TABLE coaches (
     title char(20),
     PRIMARY KEY (staff_id),
     FOREIGN KEY (staff_id)
-        REFERENCES staffInTeam
+        REFERENCES staff_in_team
 );
 
 CREATE TABLE managers (
@@ -71,22 +71,22 @@ CREATE TABLE managers (
     t_name char(20),
     PRIMARY KEY (staff_id),
     FOREIGN KEY (staff_id)
-        REFERENCES staffInTeam
+        REFERENCES staff_in_team
 );
 
-CREATE TABLE matchesPlayedByTeams(
- 	t_name char(20) NOT NULL,
+CREATE TABLE matches_played_by_teams(
+    t_name char(20) NOT NULL,
     match_id int NOT NULL,
- 	location char(20),
+    location char(20),
     result char(4) NOT NULL,
- 	time int,
-	date date,
- 	goals int,
-	numyellowCards int,
-	numredCards int,
-	PRIMARY KEY (t_name, match_id),
+    time int,
+    date date,
+    goals int,
+    numyellowCards int,
+    numredCards int,
+    PRIMARY KEY (t_name, match_id),
     FOREIGN KEY (t_name)
-        REFERENCES teamsInLeague
+        REFERENCES teams_in_league
 	        ON DELETE CASCADE
 );
 
@@ -100,7 +100,7 @@ CREATE TABLE updatedBy (
     FOREIGN KEY (staffId)
         REFERENCES managers,
     FOREIGN KEY (uname, jerseyNum)
-        REFERENCES playersInTeam
+        REFERENCES players_in_team
 );
 =======
     PRIMARY KEY (staff_id),
@@ -113,4 +113,4 @@ CREATE TABLE updatedBy (
     FOREIGN KEY (staff_id)
         REFERENCES managers
 );
->>>>>>> origin/master
+
